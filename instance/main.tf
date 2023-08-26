9b947d30bbd3731f0897a198d01ce35259bf1305
Alvis <cristian.david.alvis@accenture.com>
2023-08-25 21:43:30 -0500

    add egress

--- instance/main.tf
+++ instance/main.tf
@@ -15,6 +15,17 @@ resource "aws_security_group" "ssh_conection" {
         }
 
     }
+    dynamic "egress" {
+        for_each = var.egress_rules
+        content {
+            from_port = egress.value.from_port
+            to_port = egress.value.to_port
+            protocol = egress.value.protocol
+            cidr_blocks = egress.value.cidr_blocks
+
+        }
+
+    }
 
 }
 
