5d275a35e0b8afd901fe704a0d95cf2abe3ba2bd
Alvis <cristian.david.alvis@accenture.com>
2023-08-25 20:59:13 -0500

    Creacion de modulos

---
+++ instance/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "us-east-1"
+
+}
+resource "aws_security_group" "ssh_conection" {
+    name = var.sg_name    
+    dynamic "ingress" {
+        for_each = var.ingress_rules
+        content {
+            from_port = ingress.value.from_port
+            to_port = ingress.value.to_port
+            protocol = ingress.value.protocol
+            cidr_blocks = ingress.value.cidr_blocks
+
+        }
+
+    }
+
+}
+
+resource "aws_instance" "platzi-instance" {
+    #ami = "ami-0cbcccf0f00726605"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags = var.tags
+    security_groups = ["${aws_security_group.ssh_conection.name}"]
+}
---
+++ instance/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+    default=""
+    description="imagen ami"
+}
+variable "instance_type" {
+    
+}
+variable "tags" {
+    type = map
+}
+
+variable "sg_name" {
+}
+variable "ingress_rules" {
+}
